664f7e5e3e2ea6d03b9376300b90861d8ad22091
dobroshtan <aleksey.dobroshtan3579@gmail.com>
2022-02-05 20:14:14 +0200

    - enabled distributed config for gateway and service-a services - added dns support for services (but how to access to service via uri? It's possible only to 'dig'..)

--- service_A/src/main/java/test/microservices/service_a/ServiceAApplication.java
+++ service_A/src/main/java/test/microservices/service_a/ServiceAApplication.java
@@ -2,8 +2,10 @@ package test.microservices.service_a;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @SpringBootApplication
+@RefreshScope
 public class ServiceAApplication {
 
     public static void main(String[] args) {
