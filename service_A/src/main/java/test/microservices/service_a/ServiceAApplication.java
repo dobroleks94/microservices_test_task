7145a2af2d102d64b00c2a471f4d6bd3c3b87997
dobroshtan <aleksey.dobroshtan3579@gmail.com>
2022-02-05 23:18:51 +0200

    - reverted to simple configs - deployed gateway and service-a services without distributed config

--- service_A/src/main/java/test/microservices/service_a/ServiceAApplication.java
+++ service_A/src/main/java/test/microservices/service_a/ServiceAApplication.java
@@ -2,10 +2,8 @@ package test.microservices.service_a;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @SpringBootApplication
-@RefreshScope
 public class ServiceAApplication {
 
     public static void main(String[] args) {
